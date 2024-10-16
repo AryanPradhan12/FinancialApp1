9954fc31044819588914289abd5bd44059f99d03
AryanPradhan <Aryanrp2007@gmail.com>
2024-10-16 17:37:06 +0400

    First commit

---
+++ FinancialApp1/FinancialApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialApp1App.swift
+//  FinancialApp1
+//
+//  Created by Aryan Pradhan on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FinancialApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
